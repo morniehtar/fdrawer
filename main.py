0a59d7785a5c01eda62409af779a3e1ee79e95ed
morniehtar <46871606+morniehtar@users.noreply.github.com>
2022-07-09 19:37:11 +0300

    working complex function drawer

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import plotter as pltr
 
 xbnd = (-1, 1)
 ybnd = (-1, 1)
-N = 1000
+N = 1296
 
-def foo(x, y):
-    return x**2+y**2
+def foo(x):
+    return x**x
 
 
 n = int(np.sqrt(N))
 xarg = np.linspace(xbnd[0], xbnd[1], n)
 yarg = np.linspace(ybnd[0], ybnd[1], n)
 
-pltr.draw3D(xarg, yarg, foo)
+pltr.draw_ampl(xarg, yarg, foo)
+#pltr.draw_imag(xarg, yarg, foo)
--- plotter.py
+++ plotter.py
@@ -3,33 +3,91 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 mpl.use('QtCairo')
 
+def draw_ampl(x, y, z):
+    xx, yy = np.meshgrid(x, y)
+    i = 0+1j
+    zz = np.abs(z(xx + i*yy))
 
-def draw3D(x, y, z):
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
-    xx, yy = np.meshgrid(x, y)
-    zz = z(xx, yy)
-    #zz = np.sqrt(xx**2 + yy**2)
-    
     ax.set_xlabel('x')
     ax.set_ylabel('y')
-    ax.set_zlabel('Re f(x)')
+    ax.set_zlabel('z')
+    ax.set_title('|f(x+iy)|')
 
     sfc = ax.plot_surface(xx, yy, zz, cmap=mpl.cm.coolwarm, linewidth=0, antialiased=False)
-    fig.colorbar(sfc, shrink=0.5, aspect=5)
+    #fig.colorbar(sfc, shrink=0.5, aspect=5)
     
     plt.tight_layout()
     plt.show()
 
 
-def draw2D(x, y):
+def draw_real(x, y, z):
+    xx, yy = np.meshgrid(x, y)
+    i = 0+1j
+    zz = z(xx + i*yy).real
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.set_title('Re f(x+iy)')
+
+    sfc = ax.plot_surface(xx, yy, zz, cmap=mpl.cm.coolwarm, linewidth=0, antialiased=False)
+    #fig.colorbar(sfc, shrink=0.5, aspect=5)
+    
+    plt.tight_layout()
+    plt.show()
+
+
+def draw_imag(x, y, z):
+    xx, yy = np.meshgrid(x, y)
+    i = 0+1j
+    zz = z(xx + i*yy).imag
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.set_title('Im f(x+iy)')
+
+    sfc = ax.plot_surface(xx, yy, zz, cmap=mpl.cm.coolwarm, linewidth=0, antialiased=False)
+    #fig.colorbar(sfc, shrink=0.5, aspect=5)
+    
+    plt.tight_layout()
+    plt.show()
+
+
+def draw_surface(x, y, z):
+    xx, yy = np.meshgrid(x, y)
+    zz = z(xx, yy)
+
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.set_title('f(x, y)')
+
+    sfc = ax.plot_surface(xx, yy, zz, cmap=mpl.cm.coolwarm, linewidth=0, antialiased=False)
+    #fig.colorbar(sfc, shrink=0.5, aspect=5)
+    
+    plt.tight_layout()
+    plt.show()
+
+
+def draw_plot(x, y):
     yy = y(x)
 
-    plt.plot(x, yy, label='f(x)', c='lime')
+    fig, ax = plt.subplots()
 
-    plt.xlabel('x')
-    plt.grid()
-    plt.legend()
+    ax.plot(x, yy, label='f(x)', c='lime')
+
+    ax.set_xlabel('x')
+    ax.grid()
+    ax.legend()
 
     plt.tight_layout()
     plt.show()
